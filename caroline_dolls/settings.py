318624742738419e817a7c0f57d5e9be8c1809f3
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-07-29 00:42:20 +0100

    seoond full commit

--- caroline_dolls/settings.py
+++ caroline_dolls/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # SECURITY
 #SECURE_SSL_REDIRECT = True  # Force HTTPS
