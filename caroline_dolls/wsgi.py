3a4991eb6b18def4a3748a7b741467f6ff9c4b61
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-07-29 09:36:42 +0100

    Add auto superuser creation

--- caroline_dolls/wsgi.py
+++ caroline_dolls/wsgi.py
@@ -8,26 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caroline_dolls.settings')
 
 application = get_wsgi_application()
 
-
-
-import django
-from django.contrib.auth import get_user_model
-
-django.setup()
-User = get_user_model()
-
-username = "hakheem"
-email = "hakheemwyatt2@gmail.com"
-password = "holli123.J"
-
-# Only create if not already created
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print("✅ Superuser created!")
 
