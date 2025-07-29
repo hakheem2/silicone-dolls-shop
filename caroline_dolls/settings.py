0d187c3461f07092d7dd702b600acf11212411e1
Anyeng Mondeson <anyengmondesonmbaubeh@gmail.com>
2025-07-29 08:19:00 +0100

    firx for env and settings allowed variables

--- caroline_dolls/settings.py
+++ caroline_dolls/settings.py
@@ -26,11 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS_ENV = os.getenv('ALLOWED_HOSTS', '')
-if ALLOWED_HOSTS_ENV:
-    ALLOWED_HOSTS = ALLOWED_HOSTS_ENV.split(',')
-else:
-    ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # SECURITY
